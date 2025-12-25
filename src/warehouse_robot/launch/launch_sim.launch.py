320d11ebad82efa1fe602ff022cac8cbfbba3230
Mannava Daasaradhi <daasaradhimannava@gmail.com>
2025-12-25 12:16:37 +0530

    Added scan topic to bridge

--- src/warehouse_robot/launch/launch_sim.launch.py
+++ src/warehouse_robot/launch/launch_sim.launch.py
@@ -34,7 +34,6 @@ def generate_launch_description():
 
 
     # Launch Gazebo Harmonic
-    # We now point to our custom world file
     world_path = os.path.join(pkg_share, 'worlds', 'warehouse.sdf')
     
     gazebo = IncludeLaunchDescription(
@@ -56,13 +55,15 @@ def generate_launch_description():
     )
 
     # Bridge
+    # Added /scan for Lidar
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
                    '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
                    '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
-                   '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V'],
+                   '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
+                   '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan'],
         output='screen'
     )
 
